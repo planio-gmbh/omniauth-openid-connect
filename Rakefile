9d06c02af44fe4f7aaae2d2258046d4995467da5
Hisashi HORIKAWA <hisashi.horikawa@gmail.com>
2017-09-24 14:40:52 +0900

    for release v0.7.7

--- Rakefile
+++ Rakefile
@@ -1,10 +1,15 @@
+# -*- coding: utf-8 -*-
+
 require 'bundler/gem_tasks'
 require 'rake/testtask'
 
 Rake::TestTask.new do |t|
-  t.libs << 'lib/omniauth-openid-connect'
-  t.test_files = FileList['test/lib/omniauth/**/*_test.rb']
+  # デフォルトで'lib'は入っている.
+  #t.libs << 'lib/omniauth-openid-connect'
+  t.test_files = FileList['test/**/*_test.rb']
   t.verbose = true
 end
+desc "Run tests"
 
-task default: :test
+# 単に rake コマンドでテスト実行.
+task default: [:test]
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,22 +1,16 @@
+# -*- coding:utf-8 -*-
 
 require 'simplecov'
-
-#require 'coveralls'
-#Coveralls.wear!
+#SimpleCov.command_name 'test'
+SimpleCov.start  # 最初に実行すること.
 
 require 'minitest/autorun'
 require 'mocha/mini_test'
 require 'faker'
 require 'active_support'
-require_relative '../lib/omniauth-openid-connect'
+require 'omniauth-openid-connect'
 require_relative 'strategy_test_case'
 
 
 OmniAuth.config.test_mode = true
 OmniAuth.config.logger = Logger.new('/dev/null')
-
-SimpleCov.command_name 'test'
-SimpleCov.start
-#Coveralls.wear!
-
-
